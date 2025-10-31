8eac3cbb199b9d55dc3d73370f5d98a1f96fc2e7
timtchouamou <tim.tchouamou@gmail.com>
2025-10-31 16:46:56 -0400

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/**/*.{ts,tsx,js,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
